414b5249ccee9b56d2014852d46cf3bc6d196da1
Deric Walintukan <deric.walintukan@gmail.com>
2018-01-16 20:41:44 +0700

    Add fallback function test

--- test/bodhi_candy.js
+++ test/bodhi_candy.js
@@ -1,20 +1,32 @@
 const web3 = global.web3;
 const assert = require('chai').assert;
 const bluebird = require('bluebird');
+const TimeMachine = require('./utils/time_machine');
+const SolAssert = require('./utils/sol_assert');
 
 const BodhiCandy = artifacts.require("../contracts/BodhiCandy.sol");
-const SolAssert = require('../utils/sol_assert');
 
 const ethAsync = bluebird.promisifyAll(web3.eth);
 
 contract('BodhiCandy', (accounts) => {
+  const timeMachine = new TimeMachine(web3);
+  const getBlockNumber = bluebird.promisify(web3.eth.getBlockNumber);
+
+  let contract;
+
+  beforeEach(timeMachine.snapshot);
+  afterEach(timeMachine.revert);
+
+  beforeEach(async () => {
+    contract = await BodhiCandy.deployed();
+  });
 
   describe('fallback function', () => {
     it('throws upon sending funds to it', async () => {
       try {
         await ethAsync.sendTransactionAsync({
-          to: centralizedOracle.address,
-          from: USER1,
+          to: contract.address,
+          from: accounts[0],
           value: 1,
         });
         assert.fail();
